34ff633238d4ec3d8d7c6e243a18eb6cd28abb5d
Martin Hulten-Ashauer <martin@mortos.org>
2015-05-20 22:19:25 +1000

    use db relative paths

--- tagm_ls.py
+++ tagm_ls.py
@@ -3,7 +3,8 @@ import tagm, sys, os
 
 def main():
     # Get the tagm db
-    tdb = tagm.TagmDB( os.path.join( tagm.find_db(), '.tagm.db' ) )
+    dbpath = tagm.find_db()
+    tdb = tagm.TagmDB( os.path.join( dbpath, '.tagm.db' ) )
 
     # Work out what files to ls
     if len( sys.argv ) > 1:
@@ -14,7 +15,7 @@ def main():
         files = os.listdir( u'.' )
 
     for f in files:
-        tags = tdb.get_obj_tags( [ f ] )
+        tags = tdb.get_obj_tags( tagm.process_paths( dbpath, [ f ] ) )
         print f, tags and ','.join( [ ':'.join( t ) for t in tags ] ) or '-'
 
 if __name__ == '__main__':
