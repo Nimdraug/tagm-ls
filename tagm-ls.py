049aee348ff7aef15e94af9459944f247d2944f5
Martin Hulten-Ashauer <martin@mortos.org>
2015-05-20 21:28:46 +1000

    make ready for setup.py

--- tagm-ls.py
+++ tagm-ls.py
@@ -1,17 +1,21 @@
 #!/bin/env python2
 import tagm, sys, os
 
-# Get the tagm db
-tdb = tagm.TagmDB( os.path.join( tagm.find_db(), '.tagm.db' ) )
+def main():
+    # Get the tagm db
+    tdb = tagm.TagmDB( os.path.join( tagm.find_db(), '.tagm.db' ) )
 
-# Work out what files to ls
-if len( sys.argv ) > 1:
-    # Files provided in cmd args
-    files = sys.argv[1:]
-else:
-    # List all in dir
-    files = os.listdir( u'.' )
+    # Work out what files to ls
+    if len( sys.argv ) > 1:
+        # Files provided in cmd args
+        files = sys.argv[1:]
+    else:
+        # List all in dir
+        files = os.listdir( u'.' )
 
-for f in files:
-    tags = tdb.get_obj_tags( [ f ] )
-    print f, tags and ','.join( [ ':'.join( t ) for t in tags ] ) or '-'
+    for f in files:
+        tags = tdb.get_obj_tags( [ f ] )
+        print f, tags and ','.join( [ ':'.join( t ) for t in tags ] ) or '-'
+
+if __name__ == '__main__':
+    main()
